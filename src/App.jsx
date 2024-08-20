05637e183ba080996f366d275a3d4e2e3a3bc3ae
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-08-20 15:15:54 +0700

    makejs-for-condition-check-soldout

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,56 @@
-import '../public/index.css';
+import "../public/index.css"
 const pizzaData = [
   {
     name: "Focaccia",
     ingredients: "Bread with italian olive oil and rosemary",
     price: 6,
     photoName: "pizzas/focaccia.jpg",
-    soldOut: false,
+    soldOut: false
   },
   {
     name: "Pizza Margherita",
     ingredients: "Tomato and mozarella",
     price: 10,
     photoName: "pizzas/margherita.jpg",
-    soldOut: false,
+    soldOut: false
   },
   {
     name: "Pizza Spinaci",
     ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
     price: 12,
     photoName: "pizzas/spinaci.jpg",
-    soldOut: false,
+    soldOut: false
   },
   {
     name: "Pizza Funghi",
     ingredients: "Tomato, mozarella, mushrooms, and onion",
     price: 12,
     photoName: "pizzas/funghi.jpg",
-    soldOut: false,
+    soldOut: false
   },
   {
     name: "Pizza Salamino",
     ingredients: "Tomato, mozarella, and pepperoni",
     price: 15,
     photoName: "pizzas/salamino.jpg",
-    soldOut: true,
+    soldOut: true
   },
   {
     name: "Pizza Prosciutto",
     ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
     price: 18,
     photoName: "pizzas/prosciutto.jpg",
-    soldOut: false,
-  },
-];
+    soldOut: false
+  }
+]
 export default function App() {
   return (
     <>
-    <div className="container">
-      <Header />
-      <Menu />
-      <Footer />
-    </div>
+      <div className="container">
+        <Header />
+        <Menu />
+        <Footer />
+      </div>
     </>
   )
 }
@@ -65,15 +65,24 @@ function Header() {
   )
 }
 
-function Menu(){
+function Menu() {
+  const pizzas = pizzaData
+  // const pizzas = []
+  const numPizzas = pizzas.length
   return (
     <>
       <main className="menu">
         <h2>Our menu</h2>
 
-        <ul className='pizzas'>
-          {pizzaData.map(pizza => <Pizza pizzaObj={pizza} key={pizza.name}/>)}
-        </ul>
+        {numPizzas > 0 ? (
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        ) : (
+          <p>We're still working on our menu. Please come back later :)</p>
+        )}
         {/* <Pizza name='Pizza Spinaci' 
           ingredient='Tomato, mozarella, spinach' 
           photoName='../public/pizzas/spinaci.jpg' 
@@ -89,9 +98,12 @@ function Menu(){
   )
 }
 
-function Pizza(props){
+function Pizza(props) {
+  if (props.pizzaObj.soldOut) {
+    return null
+  }
   return (
-    <li className='pizza'>
+    <li className="pizza">
       <img src={props.pizzaObj.photoName} alt="Pizza spinaci" />
       <div>
         <h3>{props.pizzaObj.name}</h3>
@@ -104,14 +116,28 @@ function Pizza(props){
 
 function Footer() {
   const hour = new Date().getHours()
-  const openHour = 12;
-  const closeHour = 22;
-  const isOpen = hour >= openHour && hour <= closeHour;
-  
-  
+  const openHour = 12
+  const closeHour = 22
+  const isOpen = hour >= openHour && hour <= closeHour
+  // if (!isOpen) {
+  //   return <p>CLOSED</p>
+  // }
   return (
     <>
-      <footer className='footer'>{new Date().toLocaleTimeString()}. We' re currently open</footer>
+      <footer className="footer">
+        {isOpen ? (
+          <div className="order">
+            <p>
+              We're open until {closeHour}:00. Come visit us or order online.
+            </p>
+            <button className="btn">Order now</button>
+          </div>
+        ) : (
+          <p>
+            We're to welcome you between {openHour}:00 and {closeHour}:00.
+          </p>
+        )}
+      </footer>
     </>
   )
 }
